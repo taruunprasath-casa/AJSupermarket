ba316d236dd8d2236556677dabddea5dc097d703
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-08-13 13:18:30 +0530

    Taruun | created file InventoryManager to handle the inventory related functions

---
+++ Inventory/InventoryManger.ts
@@ -0,0 +1,26 @@
+import type { Product } from "../models/Product.js";
+
+export interface IInventoryManager {
+  addProduct(product: Product): void;
+  getProduct(id: string): Product | undefined;
+  updateQuantity(id: string, qty: number): void;
+}
+
+export class InventoryManager implements IInventoryManager {
+  private products = new Map<string, Product>();
+
+  addProduct(product: Product): void {
+    this.products.set(product.id, product);
+  }
+
+  getProduct(id: string): Product | undefined {
+    return this.products.get(id);
+  }
+
+  updateQuantity(id: string, qty: number): void {
+    const product = this.products.get(id);
+    if (product) {
+      product.quantity = qty;
+    }
+  }
+}
