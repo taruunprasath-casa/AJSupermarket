b843fa0aaee08c41ff34dc2e558f904c27b5fe08
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-08-18 16:27:30 +0530

    Taruun | Create Model for Offer

---
+++ models/Offer.js
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=Offer.js.map
---
+++ models/Offer.ts
@@ -0,0 +1,9 @@
+import type { SaleItem } from "./SaleItem.js";
+
+export interface Offer {
+  id: string;                           
+  code: string;                         
+  productId: SaleItem["productId"];     
+  minimumQuantity: number;              
+  discountPercentage: number;           
+}
