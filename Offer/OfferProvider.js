fd39487c012d6348a7915d6135d50eeaf243253e
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-08-18 16:28:07 +0530

    Taruun | Created offerProvider to handle offer related functionalities

---
+++ Offer/OfferProvider.js
@@ -0,0 +1,26 @@
+export class OfferProvider {
+    offers = [];
+    createOffer(offer) {
+        this.offers.push(offer);
+        console.log("Offer Added.");
+        return offer.code;
+    }
+    getBestOffer(productId, quantity, price) {
+        const productOffers = this?.offers.filter((o) => o.productId === productId);
+        if (productOffers.length === 0)
+            return null;
+        let bestOffer = null;
+        let maxDiscountValue = 0;
+        for (const offer of productOffers) {
+            if (quantity >= offer.minimumQuantity) {
+                const discountValue = quantity * price * (offer.discountPercentage / 100);
+                if (discountValue > maxDiscountValue) {
+                    maxDiscountValue = discountValue;
+                    bestOffer = offer;
+                }
+            }
+        }
+        return bestOffer;
+    }
+}
+//# sourceMappingURL=OfferProvider.js.map
---
+++ Offer/OfferProvider.ts
@@ -0,0 +1,45 @@
+import type { Offer } from "../models/Offer.js";
+import type { SaleItem } from "../models/SaleItem.js";
+
+export interface IOfferProvider {
+  createOffer(offer: Offer): string;
+  getBestOffer(
+    productId: SaleItem["productId"],
+    quantity: number,
+    price: number
+  ): Offer | null;
+}
+
+export class OfferProvider implements IOfferProvider {
+  private offers: Offer[] = [];
+
+  createOffer(offer: Offer): string {
+    this.offers.push(offer);
+    console.log("Offer Added.");
+    return offer.code;
+  }
+
+  getBestOffer(
+    productId: SaleItem["productId"],
+    quantity: number,
+    price: number
+  ): Offer | null{
+    const productOffers = this?.offers.filter((o) => o.productId === productId);
+    if (productOffers.length === 0) return null;
+
+    let bestOffer: Offer | null = null;
+    let maxDiscountValue = 0;
+
+    for (const offer of productOffers) {
+      if (quantity >= offer.minimumQuantity) {
+        const discountValue =
+          quantity * price * (offer.discountPercentage / 100);
+        if (discountValue > maxDiscountValue) {
+          maxDiscountValue = discountValue;
+          bestOffer = offer;
+        }
+      }
+    }
+    return bestOffer;
+  }
+}
