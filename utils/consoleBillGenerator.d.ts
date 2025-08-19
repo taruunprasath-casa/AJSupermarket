00a9189b330d7c5212df0ad601ddc51d851eb030
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-08-19 10:14:30 +0530

    Taruun | Renaming of billgenerator

--- utils/consoleBillGenerator.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Bill } from "../models/Bill.js";
-export declare class ConsoleBillGenerator {
-    static printBill(bill: Bill): void;
-}
-//# sourceMappingURL=consoleBillGenerator.d.ts.map
